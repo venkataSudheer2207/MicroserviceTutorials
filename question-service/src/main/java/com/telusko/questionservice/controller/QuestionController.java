27c320f7747a7f36d6446467f9ed0aedd4714a75
Sudheer <venkatsudheer7777@gmail.com>
2026-04-02 17:42:09 +0530

    code is changed

--- question-service/src/main/java/com/telusko/questionservice/controller/QuestionController.java
+++ question-service/src/main/java/com/telusko/questionservice/controller/QuestionController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//new line
 @RestController
 @RequestMapping("question")
 public class QuestionController {
